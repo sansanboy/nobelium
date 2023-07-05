d5f48466eb3201a0ff7a61fef92b5ef927b5ed36
JimYan <sansanboy@users.noreply.github.com>
2023-07-05 17:29:50 +0800

    Update blog.config.js

--- blog.config.js
+++ blog.config.js
@@ -1,5 +1,5 @@
 const BLOG = {
-  title: 'Jim Blog',
+  title: 'JimBlog',
   author: 'JimYan',
   email: 'nefu.yzj@gmail.com',
   link: 'https://www.dajun.info',
@@ -20,7 +20,7 @@ const BLOG = {
   ogImageGenerateURL: 'https://og-image-craigary.vercel.app', // The link to generate OG image, don't end with a slash
   socialLink: 'https://twitter.com/MoMaDa2022',
   seo: {
-    keywords: ['Blog', 'Website', 'Notion'],
+    keywords: ['Blog', 'Website', '技术'],
     googleSiteVerification: '' // Remove the value or replace it with your own google site verification code
   },
   notionPageId: process.env.NOTION_PAGE_ID, // DO NOT CHANGE THIS！！！
