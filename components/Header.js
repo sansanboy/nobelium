236738afc018a2228dcca55ea46d9727dffabd6f
Jim <sansanboy@users.noreply.github.com>
2025-02-26 14:08:12 +0800

    增加 PHOTOS 菜单

--- components/Header.js
+++ components/Header.js
@@ -10,6 +10,7 @@ const NavBar = () => {
   const locale = useLocale()
   const links = [
     { id: 0, name: locale.NAV.INDEX, to: BLOG.path || '/', show: true },
+    { id: 1, name: locale.NAV.PHOTOS, to: 'https://photos.dajun.info/', show: true },
     { id: 1, name: locale.NAV.ABOUT, to: '/about', show: BLOG.showAbout },
     { id: 4, name: locale.NAV.WEIBO, to: '/weibo', show: BLOG.showWeibo },
     { id: 2, name: locale.NAV.RSS, to: '/feed', show: true, external: true },
